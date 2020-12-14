7d084f671ba0f2d0e43299ea9a055d2030021c86
Matthew Dong <32016991+matt-dong@users.noreply.github.com>
2020-12-13 21:30:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ flags.DEFINE_integer("test_batch_size", 1024, "The size of batch images for test
 flags.DEFINE_integer("image_size", 33, "The size of image to use [33]")
 flags.DEFINE_integer("label_size", 33, "The size of label to produce [33]")
 flags.DEFINE_integer("model_label_size", 33, "for model loading [33]")
-flags.DEFINE_integer("patience", 15, "The steps for early stop [10]")
-flags.DEFINE_float("learning_rate", 3e-4, "The learning rate of gradient descent algorithm [1e-4]")
+flags.DEFINE_integer("patience", 15, "The steps for early stop [15]")
+flags.DEFINE_float("learning_rate", 3e-4, "The learning rate of gradient descent algorithm [3e-4]")
 #flags.DEFINE_float("momentum",0.9,"The momentum of SGD [0.9]")###add momentum for better training performance
 flags.DEFINE_integer("c_dim", 9, "Dimension of image color. [9]")
 flags.DEFINE_integer("scale", 3, "The size of scale factor for preprocessing input image [3]")
-flags.DEFINE_integer("stride", 33, "The size of stride to apply input image [14]")
+flags.DEFINE_integer("stride", 33, "The size of stride to apply input image [33]")
 flags.DEFINE_string("checkpoint_dir", "checkpoint/General", "Name of checkpoint directory [checkpoint]")
 flags.DEFINE_string("summary_dir", "saved_model", "Name of tensorboard directory [checkpoint]")
 flags.DEFINE_string("trn_folderpath", "Train/CT", "Name of sample directory [sample]")
 flags.DEFINE_string("tst_folderpath", "Test/Set5", "Name of sample directory [sample]")
 flags.DEFINE_boolean("is_train", True, "True for training, False for testing [True]")
 flags.DEFINE_string("new_image_path","Test/Set5","Path of your image to test")
-flags.DEFINE_boolean("make_patch",False,"generate patches even if h5 already exists [True]")
+flags.DEFINE_boolean("make_patch",False,"generate patches even if h5 already exists [False]")
 
 FLAGS = flags.FLAGS
 
